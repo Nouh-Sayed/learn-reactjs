a8c248f63fdb5e39dcb5213e0765f76192a3e730
Nouh Sayed <nouhsayed8@gmail.com>
2026-08-08 21:16:46 +0300

    this is the first hour in the course

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+const App = () => {
+  return (
+    <div className="bg-gray-300 p-5 w-100 h-100 ">
+      <div className=" flex justify-start gap-6 pb-5 border-b border-b-4 border-blue-900">
+      <img src="src\assets\react.svg" alt="" />
+      <h1 className="text-3xl font-bold  ">
+        this is the first element
+      </h1>
+      </div>
+      <div className=" mt-20">
+      <h3 className="font-bold ">what can you do with react</h3>
+      <ol className="list-disc pl-5">
+        <li>jdshkfhai</li>
+        <li>df;dsaf;ljsadf</li>
+        <li>jkdsjdfgsglkgj</li>
+        <li>kjhfdgkjfkjhgdhgkjfd</li>
+        <li>fkjhsudhfiugfdiug</li>
+      </ol></div>
+    </div>
+  )
+}
+
+export default App
