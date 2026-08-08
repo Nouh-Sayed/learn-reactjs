aa956bfcf2c1c7ba42d6bcc7ff835a19a1e02d81
Nouh Sayed <nouhsayed8@gmail.com>
2026-08-08 21:21:59 +0300

    then 1 hour

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ const App = () => {
       <div className=" mt-20">
       <h3 className="font-bold ">what can you do with react</h3>
       <ol className="list-disc pl-5">
-        <li>jdshkfhai</li>
-        <li>df;dsaf;ljsadf</li>
-        <li>jkdsjdfgsglkgj</li>
-        <li>kjhfdgkjfkjhgdhgkjfd</li>
-        <li>fkjhsudhfiugfdiug</li>
+        <li>reason 1 </li>
+        <li>reason 2 </li>
+        <li>reason 3 </li>
+        <li>reason 4 </li>
+        <li>reason 5 </li>
       </ol></div>
     </div>
   )
